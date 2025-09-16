26e1cc9ba726aa14aee93cdefd891aae94d34322
AMoose2048 <amoose2048@gmail.com>
2025-09-16 13:42:00 +0200

    Testing TW_HIDE_MOUSE_CURSOR

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -192,6 +192,7 @@ TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := false
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := hbtp_vm
+TW_HIDE_MOUSE_CURSOR := true
 TW_FRAMEBUFFER_SWAP_RED_BLUE := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 255
